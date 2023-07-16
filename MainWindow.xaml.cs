542e69b7dc2c7d315ce00dea59d0d03384fe43d7
AwakusuKai <61651870+AwakusuKai@users.noreply.github.com>
2023-07-16 18:31:07 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTraining
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach(UIElement uIElement in MainGrid.Children)
+            {
+                if(uIElement is Button)
+                {
+                    ((Button)uIElement).Click += Button_Click;
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "Очистить")
+                Display.Text = "";
+            else if(str == "=")
+            {
+                string value = new DataTable().Compute(Display.Text, null).ToString();
+                Display.Text = value;
+            }
+            else
+                Display.Text = Display.Text += str;
+        }
+    }
+}
